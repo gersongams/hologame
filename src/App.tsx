cfe8722fbf685e56a87cad88231448fb983330a1
Gerson Garrido <ggarridom23@gmail.com>
2021-03-25 15:10:31 -0500

    :fire: Setting project with phaser and react

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Demo from './phaser'
+
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#125555',
+  width: 800,
+  height: 600,
+  scene: Demo
+};
+
+const game = new Phaser.Game(config);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
